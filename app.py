546da8894944d57929127868d6653e4e39573081
Sayali Shelke <57850705+sayali1004@users.noreply.github.com>
2025-05-11 14:33:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("Provide the following information to check for PCOS:")
 age = st.number_input("Age", min_value=10, max_value=50, step=1)
 weight = st.number_input("Weight (Kg)", min_value=30.0, max_value=200.0)
 height = st.number_input("Height(cm) ", min_value=120.0, max_value=210.0)
-cycle_length = st.number_input("Cycle length(days)", min_value=10, max_value=45)
+cycle_length = st.number_input("Cycle length(days)", min_value=10, max_value=100)
 follicle_count = st.number_input("Follicle No. (R)", min_value=0, max_value=50)
 
 # Prepare input for model
