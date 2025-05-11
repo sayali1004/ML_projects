fb72e77775a406a4ae5acaa1672eecb2ae2810ad
Sayali Shelke <57850705+sayali1004@users.noreply.github.com>
2025-05-11 15:01:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,29 @@ st.title("PCOS Prediction App")
 st.write("Provide the following information to check for PCOS:")
 
 #input features
-age = st.number_input("Age", min_value=10, max_value=50, step=1)
-weight = st.number_input("Weight (Kg)", min_value=30.0, max_value=200.0)
-height = st.number_input("Height(cm) ", min_value=120.0, max_value=210.0)
 cycle_length = st.number_input("Cycle length(days)", min_value=10, max_value=100)
-follicle_count = st.number_input("Follicle No. (R)", min_value=0, max_value=50)
+follicle_count_right = st.number_input("Follicle No. (R)", min_value=0, max_value=50)
+follicle_count_left = st.number_input("Follicle No. (L)", min_value=0, max_value=50)
+AMH = st.number_input("AMH Level", min_value=0.0, max_value=15.0)
+
+
+# Yes/No binary features
+Pimples = st.radio("Pimples?", ["Yes", "No"])
+hair_growth = st.radio("Hair Growth?", ["Yes", "No"])
+weight_gain = st.radio("Weight Gain?", ["Yes", "No"])
+skin_Darkening=st.radio("Skin Darkening?", ["Yes", "No"])
+Fast_Food=st.radio("Fast Food?",["Yes","No"])
+
+
+# Convert Yes/No to 1/0
+irregular_periods_val = 1 if irregular_periods == "Yes" else 0
+hair_growth_val = 1 if hair_growth == "Yes" else 0
+weight_gain_val = 1 if weight_gain == "Yes" else 0
 
 # Prepare input for model
 if st.button("Predict"):
     # You may need to match the exact feature order from training
-    input_data = np.array([[age, weight, height, cycle_length, follicle_count]])
+    input_data = np.array([[cycle_length, follicle_count_right,follicle_count_left, AMH, Pimples, hair_growth, weight_gain, skin_Darkening,Fast_Food]])
     
     prediction = model.predict(input_data)
 
